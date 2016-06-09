41ded3e146273063897dd6f576c9b6234e73512a
Jan Everaert <EveraertJan@users.noreply.github.com>
2016-06-09 16:41:22 +0200

    adding full readout

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -5,7 +5,7 @@ void ofApp::setup(){
 	ofSetVerticalSync(true);
 	
 	ofBackground(255);	
-	ofSetLogLevel(OF_LOG_VERBOSE);
+//	ofSetLogLevel(OF_LOG_VERBOSE);
     
     client.connect("echo.websocket.org");
 	
@@ -15,10 +15,10 @@ void ofApp::setup(){
 	// this should be set to whatever com port your serial device is connected to.
 	// (ie, COM4 on a pc, /dev/tty.... on linux, /dev/tty... on a mac)
 	// arduino users check in arduino app....
-	int baud = 9600;
-	serial.setup(0, baud); //open the first device
+	int baud = 115200;
+	//serial.setup(1, baud); //open the first device
 	//serial.setup("COM4", baud); // windows example
-	//serial.setup("/dev/tty.usbserial-A4001JEC", baud); // mac osx example
+	serial.setup("/dev/tty.usbserial-FTG7QFLS", baud); // mac osx example
 	//serial.setup("/dev/ttyUSB0", baud); //linux example
 	
 	nTimesRead = 0;
@@ -39,7 +39,7 @@ void ofApp::update(){
 	// otherwise, we may have a "lag" if we don't read fast enough
 	// or just read three every time. now, we will be sure to
 	// read as much as we can in groups of three...
-	
+	/*
 	nTimesRead = 0;
 	nBytesRead = 0;
 	int nRead  = 0;  // a temp variable to keep count per read
@@ -54,27 +54,67 @@ void ofApp::update(){
 		nBytesRead = nRead;
 	};
 		
-	memcpy(bytesReadString, bytesReturned, 3);
-	readTime = ofGetElapsedTimef();
+	memcpy(bytesReadString, bytesReturned, 3);*/
+	
+    
+    // we want to read the 52 bytes
+    int bytesRequired = 52;
+    unsigned char bytes[bytesRequired];
+    int bytesRemaining = bytesRequired;
+    // loop until we've read everything
+    while ( bytesRemaining > 0 )
+    {
+        // check for data
+        if ( serial.available() > 0 )
+        {
+            // try to read - note offset into the bytes[] array, this is so
+            // that we don't overwrite the bytes we already have
+            int bytesArrayOffset = bytesRequired - bytesRemaining;
+            int result = serial.readBytes( &bytes[bytesArrayOffset],
+                                          bytesRemaining );
+            
+            // check for error code
+            if ( result == OF_SERIAL_ERROR )
+            {
+                // something bad happened
+                ofLog( OF_LOG_ERROR, "unrecoverable error reading from serial" );
+                // bail out
+                break;
+            }
+            else if ( result == OF_SERIAL_NO_DATA )
+            {
+                // nothing was read, try again
+            }
+            else
+            {
+                // we read some data!
+                bytesRemaining -= result;
+                for(int i = 0; i<bytesArrayOffset; i++){
+                    cout << "bytes: " << ofToHex( bytes[i] ) << "\n";
+                }
+                cout << "end stream" << "\n";
+            }
+            
+        }
+    }
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-	if (nBytesRead > 0 && ((ofGetElapsedTimef() - readTime) < 0.5f)){
+	if (nBytesRead > 0){
 		ofSetColor(0);
 	} else {
 		ofSetColor(220);
-	}
+	}/*
 	string msg;
 	msg += "click to test serial:\n";
-	msg += "nBytes read " + ofToString(nBytesRead) + "\n";
-	msg += "nTimes read " + ofToString(nTimesRead) + "\n";
 	msg += "read: " + ofToString(bytesReadString) + "\n";
-	msg += "(at time " + ofToString(readTime, 3) + ")";
-    cout << msg << "\n";
-    
+    if(ofToHex(bytesReadString) != "00000000"){
+        cout << bytesReadString << "\n";
+    }*/
     ofDrawBitmapString(client.isConnected() ? "Client is connected" : "Client disconnected :(", 10,50);
-    ofDrawBitmapString(msg, 10, 80);
+    //ofDrawBitmapString(msg, 10, 80);
 }
 //--------------------------------------------------------------
 void ofApp::onConnect( ofxLibwebsockets::Event& args ){
